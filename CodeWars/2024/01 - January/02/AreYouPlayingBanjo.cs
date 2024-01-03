7dd36c5050145d8398bf7b8d0c6d82fc0280e64d
MIKHAIL BARDZIUHOU <91759981+bardugovmk@users.noreply.github.com>
2024-01-03 14:35:52 +0300

    I solved the task yesterday.

---
+++ CodeWars/2024/01 - January/02/AreYouPlayingBanjo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars._2024._01___January._02
+{
+    internal class Kata
+    {
+        public static string AreYouPlayingBanjo(string name)
+        {
+            if (name[0] == 'r' || name[0] == 'R')
+            {
+                return name + " plays banjo";
+            }
+            else
+            {
+                return name + " does not play banjo";
+            }
+        }
+    }
+}

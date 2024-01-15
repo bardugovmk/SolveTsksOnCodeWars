9fd7888080a8cfb096c64f5f7ac6f713059aaebd
Michael +1 <bardugovmk@gmail.com>
2024-01-15 13:40:29 +0300

    Тест 2

--- CodeWars/2024/01 - January/15/Test.cs
+++ CodeWars/2024/01 - January/15/Test.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,9 @@ namespace CodeWars._2024._01___January._15
 {
     internal class Test
     {
+        public void Write()
+        {
+            Console.WriteLine(1);
+        }
     }
 }

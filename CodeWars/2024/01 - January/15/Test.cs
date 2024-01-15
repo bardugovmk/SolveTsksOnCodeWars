5fb4bb01a276994bfc326feb69ea1f00d6f3c8ad
Michael +1 <bardugovmk@gmail.com>
2024-01-15 13:32:21 +0300

    Тестовая загрузка

---
+++ CodeWars/2024/01 - January/15/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars._2024._01___January._15
+{
+    internal class Test
+    {
+    }
+}
